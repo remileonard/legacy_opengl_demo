bc1791e416511c86a85526e39ef6d9eda706b64f
Rémi Léonard <rpy.leonard@gmail.com>
2025-10-21 20:22:13 +0200

    add buttonfly

---
+++ src/buttonfly/graph.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <Windows.h>
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <gl/glut.h>
+
+#define FLAT			1
+#define DEFMATERIAL		2
+#define DEFLIGHT		3
+#define DEFLMODEL		4
+#define REDRAW			5
+
+
+
+#define ESCKEY			10
+#define WINQUIT			11
+#define MIDDLEMOUSE		12
+#define MOUSEX			13
+#define MOUSEY			14
+#define LEFTMOUSE		15
+#define RIGHTMOUSE		16
+
+#define CLOCK_MONOTONIC 20
